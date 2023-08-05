582bb139d127afb470e0e34ba357ac961a3243f4
BunBaoi <135566105+BunBaoi@users.noreply.github.com>
2023-08-05 16:26:03 +1000

    Complicated but i guessed fixed

--- Assets/Scripts/Movement&Character/PlayerController.cs
+++ Assets/Scripts/Movement&Character/PlayerController.cs
@@ -32,11 +32,6 @@ public class PlayerController : MonoBehaviour
     private PlayerStatisticsDisplay statsDisplay;
 
 
-    Animator animator;
-    int isWalkingHash;
-    int isRunningHash;
-    private PlayerInput playerInput;
-
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -45,11 +40,6 @@ public class PlayerController : MonoBehaviour
         originalCameraPosition = playerCamera.transform.localPosition;
         playerHealth = GetComponent<Health>();
         statsDisplay = FindObjectOfType<PlayerStatisticsDisplay>();
-        playerInput = GetComponent<PlayerInput>();
-
-        animator = GetComponent<Animator>();
-        isWalkingHash = Animator.StringToHash("isWalking");
-        isRunningHash = Animator.StringToHash("isRunning");
     }
 
     private void Update()
@@ -60,30 +50,16 @@ public class PlayerController : MonoBehaviour
         gamepad = Gamepad.all[playerIndex];
 
         float speed = walkSpeed;
-
-        bool isWalking = animator.GetBool(isWalkingHash);
-        bool isRunning = animator.GetBool(isRunningHash);
-
-
-        bool runPressed = gamepad.leftStickButton.isPressed; // Running
-
-        HandleInput();
-        HandleMovement();
-
-
-
-        // If gamepad is true and player runs
-        if (isRunning && (runPressed || fowardPressed))
+        if (gamepad.leftStickButton.isPressed)
         {
             speed = sprintSpeed;
-            animator.SetBool(isRunningHash, true);
-        }
-        // If gamepad is false then player stops running
-        if (isRunning && (!runPressed || !fowardPressed))
-        {
-            animator.SetBool(isRunningHash, false);
         }
 
+        Vector2 stickInput = gamepad.leftStick.ReadValue();
+        Vector3 input = new Vector3(stickInput.x, 0, stickInput.y);
+        Vector3 direction = transform.TransformDirection(input).normalized;
+        Vector3 horizontalMovement = direction * speed;
+        Vector3 verticalMovement = Vector3.up * velocity.y;
 
 
         if (controller.isGrounded)
@@ -139,7 +115,7 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        
+        controller.Move((horizontalMovement + verticalMovement) * Time.deltaTime);
 
 
         Vector2 lookInput = gamepad.rightStick.ReadValue();
@@ -163,22 +139,6 @@ public class PlayerController : MonoBehaviour
             }
             Die();
         }
-    }
-
-    void HandleInput()
-    {
-        Vector2 stickInput = gamepad.leftStick.ReadValue();
-        Vector3 input = new Vector3(stickInput.x, 0, stickInput.y);
-        Vector3 direction = transform.TransformDirection(input).normalized;
-        Vector3 horizontalMovement = direction * speed;
-
-        controller.Move(input * Time.deltaTime * speed);
-
-    }
-
-    void HandleMovement()
-    {
-        Vector3 move = new Vector3(movement().x, HandleMovement,y);
     }
 
     public void Die()
